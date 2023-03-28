3d97af36737a6ab59f0d0c85a23c27e1e2936a5e
arash foroutan <arash.foroutan@gmail.com>
2023-03-28 18:41:22 +0430

    Parameters Setting - Hybrid

---
+++ Parameters Setting/Hybrid/09/HybridSAproblem.m
@@ -0,0 +1,99 @@
+clc;
+clear;
+close all;
+tic;
+%% Definition problem
+
+Input=CreateInput;
+[Ynew , NewPrecedency]=RepairSolution(RandomSolution(Input),Input);
+CostFunction=@(x) SchedulingCost(Ynew,NewPrecedency,Input);
+%% SA parameters
+
+Maxit=100;        %maximume number of iteration
+Maxsubit=15;       %maximume number of iteration
+T0=150;            % number of temp
+alpha=0.96;
+Tabulistsize=4;
+% Neighborsize=40;
+
+%% Initialization
+
+TabuList=[];
+Archive=[];
+
+x.position=Ynew;
+x.cost=CostFunction(x.position);
+
+x.N=[];       % number of individuals that dominance 'x'       
+
+Archive=[Archive x];
+TabuList=[TabuList x];
+% store temp
+T=T0;
+
+
+%% Hybrid Main loop
+
+for it=1:Maxit
+    
+   for subit1=1:Maxsubit
+       
+       %  create new solution 
+       newsol=CreateNeighbor(x,TabuList,1,Input);
+%        newsol.Z=Schedulingcost(newsol.position,Input);
+       Gsol=sum(log(x.cost));
+       Gnewsol=sum(log(newsol.cost));
+       P=exp((-1/T) * (Gnewsol - Gsol));
+       
+       if rand<=P
+          x=newsol;
+       end
+       Archive=SaveStrategy(x,Archive);
+               
+        t=numel(Archive);
+        k=randsample(t,1);
+%         sol=Archive(end);
+        x=Archive(k);
+%             figure(1);
+%             Plotsolution(Archive,it);
+   end
+   
+    % Update taboo list   
+        if numel(TabuList)==Tabulistsize
+           TabuList(end)=[]; 
+           TabuList=[x TabuList];    %#ok
+        else
+           TabuList=[x TabuList];        %#ok
+        end      
+   % update temp
+    T=alpha*T;
+    
+    % show Non-dominated Solutions
+    figure(1);
+    Plotsolution(Archive,it);
+end
+time=toc;
+
+% result
+%disp(['Number of Non-dominated Solutions = ' num2str(numel(Archive))]);
+
+
+Output=CreateOutput(Archive);
+Output.Time=time;
+save('hybrid' , 'Output');
+
+disp(['Time = ' ...
+    num2str(Output.Time)]);
+disp(['NPS = ' ...
+    num2str(Output.NPS)]);
+disp(['MID = ' ...
+    num2str(Output.MID)]);
+disp(['DM = ' ...
+    num2str(Output.DM)]);
+disp(['SNS = ' ...
+    num2str(Output.SNS)]);
+disp(['RAS = ' ...
+    num2str(Output.RAS)]);
+
+
+
