c9430a470b1bf28c89cfb5beccf41d3ae66e2243
arash.foroutan@gmail.com <arashforoutan>
2023-04-04 01:34:50 +0330

    Multiseries plot B and B

--- Multi-Series Plot/PlotCosts.m
+++ Multi-Series Plot/PlotCosts.m
@@ -4,12 +4,34 @@ function PlotCosts()
         Input.hybrid=load('hybrid.mat');
         Input.moead=load('moead.mat');
 
+        empty_individual.bandb=[];
         empty_individual.nsga2=[];
         empty_individual.hybrid=[];
         empty_individual.moead=[];
 
-        pop=repmat(empty_individual,3,1);
+        pop=repmat(empty_individual,4,1);
 
+        temp0=cell(4,1);
+        temp0{1}=[23;1.001666666666667e+02;1.808333333333333e+02];
+        temp0{2}=[20.833333333333336;1.130000000000000e+02;1.325000000000000e+02];
+        temp0{3}=[20.500000000000000;106;1.615000000000000e+02];
+        %temp0{4}=[24.3334;13.6667;132.5000];
+        for i=1:4
+            pop(i).bandb=temp0{i};
+        end
+        q1=[pop.bandb];
+        x=q1(1,:);
+        y=q1(2,:);
+        z=q1(3,:);
+        %plot3(x,y,z,'or','Markersize',15, 'marker' , '*' ,'markerfacecolor','green' , 'markeredgecolor','green');
+        plot3(x,y,z,'or','Markersize',25, 'marker' , 's' , 'markeredgecolor','magenta');
+        title(['Iteration =' 100]);
+        xlabel('f_1(x)');
+        ylabel('f_2(x)'); 
+        zlabel('f_3(x)');
+        grid on
+        hold on
+        
         temp1=[Input.nsga2.Output.Paretolist2];
         for i=1:numel(temp1)
             pop(i).nsga2=temp1{i};
@@ -37,7 +59,7 @@ function PlotCosts()
         z=qq(3,:);
         %plot3(x,y,z,'or','Markersize',6,'markerfacecolor','blue');
         %plot3(x,y,z,'or','Markersize',12, 'marker' , 'o' ,'markerfacecolor','green' , 'markeredgecolor','red');
-        plot3(x,y,z,'or','Markersize',15, 'marker' , 'o' , 'markeredgecolor','red');
+        plot3(x,y,z,'or','Markersize',15, 'marker' , 'o' , 'markeredgecolor','cyan');
         title(['Iteration =' 100]);
         xlabel('f_1(x)');
         ylabel('f_2(x)'); 
